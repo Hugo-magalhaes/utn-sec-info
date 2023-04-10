054d983a3a95b79ebd74717a72e6285ee84389a5
Fernando Rodriguez <frodriguezcora@gmail.com>
2023-04-10 11:35:05 -0300

    (ui): implement request and response interceptors to HttpHandler

--- ui/src/services/http-handler.ts
+++ ui/src/services/http-handler.ts
@@ -1,7 +1,29 @@
-import axios from 'axios'
+import axios from 'axios';
+import { useAuthStore } from '@/stores/auth-store';
+import { tokenRestore } from '@/services/token-storage';
 
-export const HttpHandler = axios.create({
+const HttpHandler = axios.create({
     baseURL: process.env.VUE_APP_SEC_API,
     timeout: 5000,
     withCredentials: true,
 });
+
+HttpHandler.interceptors.request.use(config => {
+    const token = tokenRestore();
+    config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+});
+
+HttpHandler.interceptors.response.use(
+    response => response,
+    error => {
+        if(error.response.status != 401) return error;
+        
+        const auth = useAuthStore();
+        auth.logout();
+        
+        throw Error('Login redirect');
+});
+
+export { HttpHandler };
