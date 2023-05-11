75988357f6cdd5824e4808890647585a4e4c56aa
Usuario <frodriguezcora@gmail.com>
2023-05-11 09:36:20 -0300

    (api): install packages to db connection

--- api/src/global.d.ts
+++ api/src/global.d.ts
@@ -1,7 +1,14 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        PORT: string
+        PORT: number
         UI_URL: string
         NODE_ENV: string
+        GOOGLE_APPLICATION_CREDENTIALS: string
+        DB_DIALECT: string
+        DB_HOST: string
+        DB_PORT: number
+        DB_NAME: string
+        DB_USER: string
+        DB_PASSWORD: string
     }
 }
