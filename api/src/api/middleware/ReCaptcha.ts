089df95366ade25491d0b282736e594e874bcbfd
Hugo <hugommjunior@gmail.com>
2023-05-18 09:57:04 -0300

    actualizacion del recaptcha

--- api/src/api/middleware/ReCaptcha.ts
+++ api/src/api/middleware/ReCaptcha.ts
@@ -1,31 +1,36 @@
 import axios from 'axios';
-import { Request, Response, NextFunction } from "express";
-import { UnauthorizedError } from "@api/error/unauthorized-error";
+import { Request, Response, NextFunction } from 'express';
+import { UnauthorizedError } from '@api/error/unauthorized-error';
+
 const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
 const SCORE_GOOGLE_RECAPTCHA = process.env.SCORE_GOOGLE_RECAPTCHA;
-const RECAPTCHA_ERROR_VALIDATION = "Error to aprove Recaptcha";
+const RECAPTCHA_ERROR_VALIDATION = 'Error to approve Recaptcha';
+const CAPTCHA_URL = 'https://www.google.com/recaptcha/api/siteverify';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-    const captchaURL = "https://www.google.com/recaptcha/api/siteverify";
-    const TOKEN = req.headers.authorization?.split(' ')[3] ?? "";
-    if (!TOKEN) {
-        res.status(404).send(RECAPTCHA_ERROR_VALIDATION);
+    const token = req.headers.authorization?.split(' ')[3] ?? '';
+
+    if (!token) {
+        return res.status(404).send(RECAPTCHA_ERROR_VALIDATION);
     }
+
     try {
-        const response = await axios({
-            url: captchaURL,
-            method: 'POST',
-            headers: { ContentType: 'application/x-www-form-urlencoded' },
+        const response = await axios.post(CAPTCHA_URL, null, {
             params: {
                 secret: RECAPTCHA_SECRET_KEY,
-                response: TOKEN
+                response: token
             },
         });
-        const isValidToken = response && response.data && response.data.success && SCORE_GOOGLE_RECAPTCHA && response.data.score && response.data.score > SCORE_GOOGLE_RECAPTCHA;
-        isValidToken ? next() : res.status(404).send(RECAPTCHA_ERROR_VALIDATION)
-    }
-    catch (err: any) {
+
+        const isValidToken = response?.data?.success && SCORE_GOOGLE_RECAPTCHA && response.data.score && response.data.score > SCORE_GOOGLE_RECAPTCHA;
+
+        if (isValidToken) {
+            next();
+        } else {
+            res.status(404).send(RECAPTCHA_ERROR_VALIDATION);
+        }
+    } catch (err: any) {
         console.error(err);
-        next(new UnauthorizedError("Unauthorized", err));
+        next(new UnauthorizedError('Unauthorized', err));
     }
 }
